b2dfe0738ed98ab8b565158e98b598611c27224f
marcarrera <kmcarreraprieto@gmail.com>
2024-10-10 13:08:36 -0600

    diseñando cards reportes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'utils/constants.dart';
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,16 +9,60 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = Constants.getScreenHeight(context);
+    double screenWidth = Constants.getScreenWidth(context);
+    double fontSize = Constants.getFontSize(context);
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Discipulos TID',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Buenis dias Daniela'),
+          title: const Text('Buenos dias Daniela'),
+          actions: [
+            Icon(Icons.more_vert)
+          ],
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                CardModel(screenHeight: screenHeight, screenWidth: screenWidth),
+                CardModel(screenHeight: screenHeight, screenWidth: screenWidth),
+                CardModel(screenHeight: screenHeight, screenWidth: screenWidth),
+                CardModel(screenHeight: screenHeight, screenWidth: screenWidth),
+              ],
+            ),
+          )
         ),
+      ),
+    );
+  }
+}
+
+class CardModel extends StatelessWidget {
+  const CardModel({
+    super.key,
+    required this.screenHeight,
+    required this.screenWidth,
+  });
+
+  final double screenHeight;
+  final double screenWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: screenHeight * 0.22,
+        width: screenWidth * 0.9,
+        decoration: BoxDecoration(
+          color: Constants.cardColor,
+          borderRadius: BorderRadius.circular(24)
+          
+        ),
+        child: Text('DATA'),
       ),
     );
   }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,9 +1,24 @@
 
 import 'package:flutter/material.dart';
 
-class TColors {
+class Constants {
+  //COLORS
   static Color get backgrondColor => const Color(0xFFF6F6F6);
   static Color get primaryColor => const Color(0xFF001B48);
   static Color get secondColor => const Color(0xFF004581);
   static Color get letterColor => const Color(0xFF018ABD);
+  static Color get cardColor => const Color(0xFFD9D9D9);
+
+  //SIZE
+  static double getScreenHeight(BuildContext context) {
+    return MediaQuery.of(context).size.height;
+  }
+
+  static double getScreenWidth(BuildContext context) {
+    return MediaQuery.of(context).size.width;
+  }
+
+  static double getFontSize(BuildContext context) {
+    return getScreenHeight(context) * 0.02;
+  }
 }
