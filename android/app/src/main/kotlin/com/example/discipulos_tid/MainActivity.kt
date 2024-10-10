9de7f562f3d6fd65de522e3915c136df55cbba93
marcarrera <kmcarreraprieto@gmail.com>
2024-10-10 10:42:03 -0600

    agregando proyecto

---
+++ android/app/src/main/kotlin/com/example/discipulos_tid/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.discipulos_tid
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/material.dart';
+
+class TColors {
+  static Color get backgrondColor => const Color(0xFFF6F6F6);
+  static Color get primaryColor => const Color(0xFF001B48);
+  static Color get secondColor => const Color(0xFF004581);
+  static Color get letterColor => const Color(0xFF018ABD);
+}
